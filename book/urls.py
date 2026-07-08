416131b707c059e1c7f3d5b00019e2a8d4d3f288
Kacper <kogutkacper7@gmail.com>
2026-07-08 23:41:38 +0200

    update book endpoint

--- book/urls.py
+++ book/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from book.views import BookView
 
+router = DefaultRouter()
+router.register("books", BookView, basename="book")
+
+urlpatterns = [path("", include(router.urls))]
+
+
 app_name = "book"
-
-
-urlpatterns = [
-    path("", BookView.as_view(), name="book-list")
-]
