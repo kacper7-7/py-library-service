e2ac03fc58dc53c7e1de1f7cbe74f45fb4e62cb0
Kacper <kogutkacper7@gmail.com>
2026-07-09 10:28:09 +0200

    add serializers, view and endpoint for user model

--- user/models.py
+++ user/models.py
@@ -30,8 +30,10 @@ class User(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
-    objects = CustomUserManager
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
