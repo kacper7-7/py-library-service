a61b6e2c18e7955b9c02f1cb310341c515fac70f
Kacper <kogutkacper7@gmail.com>
2026-07-09 10:39:58 +0200

    add JWT token authentication

--- library_service/settings.py
+++ library_service/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 AUTH_USER_MODEL = "user.User"
+
+
+REST_FRAMEWORK = {
+    # "DEFAULT_AUTHENTICATION_CLASSES": (
+    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
+    # ),
+    # "DEFAULT_PERMISSION_CLASSES": (
+    #     "rest_framework.permissions.IsAuthenticated"
+    # )
+}
