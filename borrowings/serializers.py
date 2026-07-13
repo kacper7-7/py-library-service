18f4e4fc47371174e82a62034828464ef526a966
Kacper <kogutkacper7@gmail.com>
2026-07-13 23:42:45 +0200

    feat: add function to automate the routine

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,15 +1,11 @@
-from django.core.mail import message
 from django.db import transaction
-from django.db.models import Model
-from rest_framework import serializers, generics
-
-from book.models import Book
+from rest_framework import serializers, generics, status
 from book.serializers import BookSerializer
 from borrowings.models import Borrowing
 from notification.models import Notification
 from notification.tasks import send_notification
 from payment.models import Payment
-from user.serializers import UserSerializer
+from payment.utils import create_stripe_session
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
@@ -40,13 +36,11 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
 
 
 class BorrowingCreateSerializer(serializers.ModelSerializer):
+    payment_url = serializers.URLField(read_only=True)
+
     class Meta:
         model = Borrowing
-        fields = [
-            "borrow_date",
-            "expected_return",
-            "book",
-        ]
+        fields = ["borrow_date", "expected_return", "book", "payment_url"]
 
     def create(self, validated_data):
         book = validated_data["book"]
@@ -61,15 +55,17 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
 
             borrowing = Borrowing.objects.create(**validated_data)
 
-            payment = Payment.objects.create(type="payment", borrowing=borrowing)
+            request = self.context.get("request")
 
-            notification = Notification.objects.create(
-                user=borrowing.user,
-                message=f"You have borrowed {borrowing.book.title}. Cost {payment.money_to_pay}$.",
-            )
-            send_notification.delay(notification.pk)
+            try:
+                payment_url = create_stripe_session(
+                    borrowing=borrowing, request=request
+                )
+                borrowing.payment_url = payment_url
 
-            return borrowing
+            except Exception as e:
+                raise serializers.ValidationError({"stripe_error": str(e)})
+        return borrowing
 
     def update(self, instance, validated_data):
         if (
