a9ccd894d23bbaff279266aac90be5d8e91a9651
Kacper <kogutkacper7@gmail.com>
2026-07-16 10:49:00 +0200

    feat: add borrowings constraints

--- borrowings/views.py
+++ borrowings/views.py
@@ -4,6 +4,7 @@ from borrowings.serializers import (
     BorrowingSerializer,
     BorrowingDetailSerializer,
     BorrowingCreateSerializer,
+    BorrowingAdminSerializer,
 )
 
 
@@ -12,16 +13,26 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
-        if self.action == "create" or self.action == "update":
+        if self.action in ["create", "update"]:
             return BorrowingCreateSerializer
+        if self.request.user.is_staff:
+            return BorrowingAdminSerializer
         return BorrowingSerializer
 
     def get_queryset(self):
-        return (
-            Borrowing.objects.select_related("book", "user")
-            .prefetch_related("payments")
-            .filter(user__id=self.request.user.pk)
-        )
+        queryset = Borrowing.objects.select_related("book").prefetch_related("payments")
+        if self.request.user.is_staff:
+            queryset = queryset.select_related("user")
+
+            user_id = self.request.query_params.get("user_id")
+
+            if user_id:
+                user_id = user_id.split(",")
+                return queryset.filter(user__id_in=user_id)
+            return queryset
+
+        else:
+            return queryset.filter(user__id=self.request.user.pk)
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
